2b2296c3f6cf7a60fc0a7787944a7274bc1806b0
Yoshiya Hinosawa <stibium121@gmail.com>
2021-05-14 18:30:57 +0900

    feat: add logging

--- install.ts
+++ install.ts
@@ -4,9 +4,11 @@ import { wasmCacheDir } from "./install_util.ts";
 await ensureDir(wasmCacheDir());
 
 const wasmPath = join(wasmCacheDir(), "esbuild.wasm")
-console.log(`Downloading esbuild wasm`);
+const wasmUrl = `https://deno.land/x/${NAME}@${VERSION}/esbuild.wasm`;
 
-const res = await fetch(`https://deno.land/x/${NAME}@${VERSION}/esbuild.wasm`);
+console.log(`Downloading esbuild wasm from ${wasmUrl}`);
+
+const res = await fetch(wasmUrl);
 const bytes = new Uint8Array(await res.arrayBuffer());
 const size = (bytes.byteLength / 1024 / 1024).toFixed(2);
 await Deno.writeFile(wasmPath, bytes);
