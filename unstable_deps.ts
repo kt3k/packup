892ac31e0b26a904efb09e3a673555185e92e932
Yoshiya Hinosawa <stibium121@gmail.com>
2021-07-31 18:01:27 +0900

    chore: update file server

--- unstable_deps.ts
+++ unstable_deps.ts
@@ -1,1 +1,1 @@
-export { serve as serveIterable } from "https://deno.land/x/iterable_file_server@v0.1.5/mod.ts";
+export { serve as serveIterable } from "https://deno.land/x/iterable_file_server@v0.1.7/mod.ts";
