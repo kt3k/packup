39401d64cba5b7be6a5b1e6ee30205baefcd238f
uki00a <uki00a@gmail.com>
2021-09-05 21:38:09 +0900

    fix(livereload_server): migrate to native http server

---
+++ livereload_server_test.ts
@@ -0,0 +1,39 @@
+import { livereloadServer } from "./livereload_server.ts";
+import {
+  assert,
+  assertEquals,
+  assertStringIncludes,
+  deferred,
+} from "./test_deps.ts";
+
+Deno.test("livereloadServer", async () => {
+  const port = 34567;
+  const eventtarget = new EventTarget();
+  const server = livereloadServer(port, eventtarget);
+  try {
+    const wsURL = `ws://localhost:${port}`;
+
+    // Livereload server should serve livereload.js
+    const res = await fetch(`http://localhost:${port}/livereload.js`);
+    const text = await res.text();
+    assertStringIncludes(text, `new WebSocket("${wsURL}/livereload")`);
+
+    // Livereload server should accept websocket request
+    const done = deferred<void>();
+    let receivedMessage: string | undefined;
+    const ws = new WebSocket(`${wsURL}/livereload`);
+    // Livereload server automatically closes itself when 'built' event is dispatched
+    ws.onclose = () => done.resolve();
+    ws.onopen = () => eventtarget.dispatchEvent(new Event("built"));
+    ws.onmessage = (ev: MessageEvent<string>) => {
+      receivedMessage = ev.data;
+    };
+    ws.onerror = (ev) => done.reject(ev);
+
+    await done;
+    assert(receivedMessage);
+    assertEquals(JSON.parse(receivedMessage), { type: "reload" });
+  } finally {
+    await server.close();
+  }
+});
