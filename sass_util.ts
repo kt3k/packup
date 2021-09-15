885316a085925784695277f2db73d87eeffccdbd
Yoshiya Hinosawa <stibium121@gmail.com>
2021-09-15 22:09:52 +0900

    feat: basic sass support (#18)

---
+++ sass_util.ts
@@ -0,0 +1,19 @@
+/** Compiles the given string into css.
+ *
+ * TODO(kt3k): Support importer
+ */
+export async function compile(text: string): Promise<string> {
+  // Use dynamic import because sass.js is slow to import
+  // Also
+  const { Sass } = await import("./vendor/sass/mod.ts");
+  return new Promise((resolve, reject) => {
+    // deno-lint-ignore no-explicit-any
+    Sass.compile(text, (result: any) => {
+      if (result.status === 0) {
+        resolve(result.text);
+      } else {
+        reject(new Error(result.message));
+      }
+    });
+  });
+}
---
+++ vendor/sass/mod.ts
@@ -0,0 +1,4 @@
+import "./sass.js";
+
+// deno-lint-ignore no-explicit-any
+export const Sass: any = (globalThis as any).Sass;
