987e85c22970b04778ff82e2254402bddf82dfd4
Yoshiya Hinosawa <stibium121@gmail.com>
2021-09-19 19:14:25 +0900

    feat: skip processing http(s):// references in html

---
+++ examples/external-reference/main.js
@@ -0,0 +1,16 @@
+$(() => {
+  $("body")
+    .css("background", "teal")
+    .css("padding", "20px")
+    .html(`
+  <header>
+    <h1>Packup Example</h1>
+  </header>
+  <main>
+    <p>This is the example of using external reference in the html
+  </main>
+  <footer>
+    This is footer
+  </footer>
+`);
+});
--- generate_assets_test.ts
+++ generate_assets_test.ts
@@ -25,3 +25,24 @@ Deno.test("extractReferencedAssets - extracts referenced assets in the html docu
   assertEquals(assets[0].constructor.name, "ScriptAsset");
   assertEquals(assets[1].constructor.name, "CssAsset");
 });
+
+Deno.test("extractReferencedAssets - references to http(s):// schemes are treated as external reference", () => {
+  const assets = [...extractReferencedAssets(
+    new DOMParser().parseFromString(
+      `
+        <html>
+          <head>
+            <title>Test document</title>
+            <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css" />
+            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+          </head>
+          <body>
+            <h1></h1>
+          </body>
+        </html>
+      `,
+      "text/html",
+    )!,
+  )];
+  assertEquals(assets.length, 0);
+});
