7ef8df2e711f4fda2450f68906dddd5a82358af6
Yoshiya Hinosawa <stibium121@gmail.com>
2021-10-19 16:58:41 +0900

    chore: update deps

--- test_deps.ts
+++ test_deps.ts
@@ -3,6 +3,6 @@ export {
   assertEquals,
   assertStringIncludes,
   assertThrows,
-} from "https://deno.land/std@0.95.0/testing/asserts.ts";
+} from "https://deno.land/std@0.112.0/testing/asserts.ts";
 
-export { deferred } from "https://deno.land/std@0.95.0/async/deferred.ts";
+export { deferred } from "https://deno.land/std@0.112.0/async/deferred.ts";
