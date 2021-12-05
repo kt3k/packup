427695c88f496a9d9ad8c0fb01593b58353f2d48
uki00a <uki00a@gmail.com>
2021-12-05 16:21:45 +0900

    feat(install_util): add support for DENO_DIR

--- install_util_test.ts
+++ install_util_test.ts
@@ -2,19 +2,35 @@ import { assertEquals } from "./test_deps.ts";
 import { join } from "./deps.ts";
 import { wasmCacheDir } from "./install_util.ts";
 
+const homedir = (os: typeof Deno.build.os) =>
+  os === "windows" ? "/userprofile" : "/home/foo";
+
 Deno.test("wasmCacheDir - returns cache dir for esbuild wasm", () => {
-  const homedir = (os: typeof Deno.build.os) =>
-    os === "windows" ? "/userprofile" : "/home/foo";
   assertEquals(
-    wasmCacheDir("windows", homedir),
+    wasmCacheDir("windows", homedir, ""),
     join("/userprofile", ".deno/packup"),
   );
   assertEquals(
-    wasmCacheDir("linux", homedir),
+    wasmCacheDir("linux", homedir, ""),
     join("/home/foo", ".deno/packup"),
   );
   assertEquals(
-    wasmCacheDir("darwin", homedir),
+    wasmCacheDir("darwin", homedir, ""),
     join("/home/foo", ".deno/packup"),
   );
+});
+
+Deno.test({
+  name: "wasmCacheDir - respects DENO_DIR if defined",
+  fn: () => {
+    const denoDir = join(Deno.cwd(), ".deno");
+    const cacheDir = join(denoDir, "packup");
+    assertEquals(wasmCacheDir("windows", homedir, denoDir), cacheDir);
+    assertEquals(wasmCacheDir("linux", homedir, denoDir), cacheDir);
+    assertEquals(wasmCacheDir("darwin", homedir, denoDir), cacheDir);
+  },
+  permissions: {
+    read: ["."],
+    env: ["DENO_DIR"],
+  },
 });
