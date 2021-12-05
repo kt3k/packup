99178578e65abdf97915a005b5973fb00380b469
Yoshiya Hinosawa <stibium121@gmail.com>
2021-12-05 18:22:26 +0900

    feat: log before and after importing sass

--- sass_util.ts
+++ sass_util.ts
@@ -4,8 +4,10 @@
  */
 export async function compile(text: string): Promise<string> {
   // Use dynamic import because sass.js is slow to import
-  // Also
+  const start = performance.now();
+  console.log("Importing sass");
   const { Sass } = await import("./vendor/" + "sass/mod.ts");
+  console.log(`Imported sass in ${(performance.now() - start).toFixed(1)}ms`);
   return new Promise((resolve, reject) => {
     // deno-lint-ignore no-explicit-any
     Sass.compile(text, (result: any) => {
