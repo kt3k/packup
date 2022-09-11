a4db7f16b33308b3714efe2b7fc1e803d6d83eb1
Yoshiya Hinosawa <stibium121@gmail.com>
2022-09-11 16:17:33 +0900

    fmt

--- examples/import-map/js/script.tsx
+++ examples/import-map/js/script.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
