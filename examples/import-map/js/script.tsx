18a7ffb9eef33b6160493cb50386e4979122c10e
Yoshiya Hinosawa <stibium121@gmail.com>
2022-09-11 14:21:21 +0900

    chore: add import-map example

---
+++ examples/import-map/js/script.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {
+          /* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */
+        }
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function main() {
+  ReactDOM.render(React.createElement(App), document.querySelector("#main"));
+}
+
+addEventListener("DOMContentLoaded", () => {
+  main();
+});
