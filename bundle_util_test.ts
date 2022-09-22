5f4524b33596bf6f917a1ec9e1697687893fb4e3
Karel Klíma <karelklima@gmail.com>
2022-09-22 12:20:10 +0200

    chore: updated esbuild to v0.14.51 (#49)

--- bundle_util_test.ts
+++ bundle_util_test.ts
@@ -1,9 +1,8 @@
 import { assertEquals, assertStringIncludes } from "./test_deps.ts";
 import { bundleByEsbuild, getImportMap, setImportMap } from "./bundle_util.ts";
-import { wasmPath } from "./install_util.ts";
 
 Deno.test("bundleByEsbuild - bundles the script by esbuild", async () => {
-  const bundle = await bundleByEsbuild("testdata/foo.js", wasmPath());
+  const bundle = await bundleByEsbuild("testdata/foo.js");
 
   assertStringIncludes(bundle, `console.log("hi");`);
 });
