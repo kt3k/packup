79e6efd47d7eeb6426b885c53fa24e8cc3372565
Yoshiya Hinosawa <stibium121@gmail.com>
2022-09-22 19:30:00 +0900

    chore: clean up

--- vendor/esbuild_deno_loader/deps.ts
+++
@@ -1,8 +0,0 @@
-import type * as esbuild from "https://deno.land/x/esbuild@v0.12.9/mod.d.ts";
-export type { esbuild };
-export {
-  fromFileUrl,
-  resolve,
-  toFileUrl,
-} from "https://deno.land/std@0.95.0/path/mod.ts";
-export * as importmap from "https://jspm.dev/@import-maps/resolve@1.0.1";
--- vendor/esbuild_deno_loader/test_deps.ts
+++
@@ -1,7 +0,0 @@
-// @deno-types="https://deno.land/x/esbuild@v0.12.9/mod.d.ts"
-import * as esbuild from "https://deno.land/x/esbuild@v0.12.9/mod.js";
-export { esbuild };
-export {
-  assert,
-  assertEquals,
-} from "https://deno.land/std@0.95.0/testing/asserts.ts";
