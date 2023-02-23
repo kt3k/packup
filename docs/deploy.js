9ac50fe8ec99ef44efe866e0edbcec846a5cd7e4
Yoshiya Hinosawa <stibium121@gmail.com>
2023-02-23 16:58:18 +0900

    chore: fix docs/deploy.js

--- docs/deploy.js
+++ docs/deploy.js
@@ -1,1 +1,4 @@
-import "https://raw.githubusercontent.com/denoland/deno_std/main/http/file_server.ts";
+import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import { serveDir } from "https://raw.githubusercontent.com/denoland/deno_std/main/http/file_server.ts";
+
+serve((req) => serveDir(req, { fsRoot: "docs" });
